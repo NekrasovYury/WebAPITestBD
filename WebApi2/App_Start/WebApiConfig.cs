c824ea2a3de914302f01eb315533ecd2c716331e
Furynoobas <samsung12345#>
2019-07-16 11:55:59 +0300

    Test

--- WebApi2/App_Start/WebApiConfig.cs
+++ WebApi2/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace WebApi2
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
                
             );
--- WebApi2/Controllers/ClientController.cs
+++ WebApi2/Controllers/ClientController.cs
@@ -13,39 +13,41 @@ namespace WebApi2.Controllers
     {
        public List<Client> clients = new List<Client>
             {
-                { new Client { ClientID = 1, ClientName = "Sergey"} },
-                {new Client { ClientID = 2, ClientName = "Yura"  } },
-                {new Client { ClientID = 3, ClientName = "Pavel" } },
-                {new Client { ClientID = 4, ClientName = "Sanya" } },
-                { new Client { ClientID = 5, ClientName = "Leha" } }
+                { new Client { ClientID = 0, ClientName = "Sergey"} },
+                {new Client { ClientID = 1, ClientName = "Yura"  } },
+                {new Client { ClientID = 2, ClientName = "Pavel" } },
+                {new Client { ClientID = 3, ClientName = "Sanya" } },
+                { new Client { ClientID = 4, ClientName = "Leha" } }
             };
 
       public  List<Card> cards = new List<Card>
         {
             
-                { new Card { CardID = 12391283 , OwnreID = 1, CardValue = 4000  } },
+                { new Card { CardID = 12391283 , OwnreID = 0, CardValue = 4000  } },
                 { new Card {CardID = 4939182 , OwnreID = 1, CardValue = 2040  } },
                 { new Card {CardID = 38491982 , OwnreID = 2, CardValue = 3100  } },
                 { new Card{ CardID = 4899182 , OwnreID = 3, CardValue = 4000  } },
                 { new Card {CardID = 4892918 , OwnreID = 3, CardValue = 5500  } },
                 { new Card{ CardID = 4899182 , OwnreID = 4, CardValue = 4020  } },
-                { new Card {CardID = 4892918 , OwnreID = 5, CardValue = 5001  } }
+                { new Card {CardID = 4892918 , OwnreID = 0, CardValue = 5001  } }
 
         };
         // GET: api/Client
+        [HttpGet]
         public List<Client> Get()
         {
             return clients;
         }
 
         // GET: api/Client/5
-        //[HttpGet]
-        //public List<Client> Get(int Id)
-        //{
-        //    string v = clients[Id].ClientName;
-
-        //    return v;
-        //}
+        [HttpGet]
+        public string Get(int Id)
+        {
+            string v = clients[Id].ClientName;
+            v += cards[clients[Id].ClientID].CardValue;
+            v += cards[clients[Id].ClientID].CardID;
+            return v;
+        }
 
         // POST: api/Client
         public void Post([FromBody]Client data)
@@ -69,9 +71,8 @@ namespace WebApi2.Controllers
         // DELETE: api/Client/5
         public void Delete(int id)
         {
-           
-            clients.RemoveAt(id);
-            cards.RemoveAt(id);
+            clients.Remove(clients[id]);
+            cards.Remove(cards[clients[id].ClientID]);
         }
     }
 }
